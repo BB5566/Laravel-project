47c626b7172acc94a94777b7d0ad14452002d751
BB <mobbsong@gmail.com>
2025-09-17 09:25:56 +0800

    更新主版型和子模板的註解，增強可讀性和使用說明

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,26 @@
 <!-- resources/views/layouts/app.blade.php -->
 
+{{--
+    主版型 (Layout)，供所有子模板繼承。
+    用法：子模板用 @extends('layouts.app') 套用本版型。
+    @yield('title')：允許子模板自訂網頁標題。
+    @section('sidebar')/@yield('sidebar')：側邊欄區塊，可被子模板覆寫，@parent 保留原內容。
+    @yield('content')：主要內容區塊，必須由子模板覆寫。
+--}}
+
 <html>
 
 <head>
-    <title>App Name - @yield('title')</title>
+    <title>App Name - @yield('title') {{-- 子模板可自訂標題 --}}</title>
 </head>
 
 <body>
-    @section('sidebar')
+    @section('sidebar') {{-- 預設 sidebar，可被子模板覆寫 --}}
         This is the master sidebar.
     @show
 
     <div class="container">
-        @yield('content')
+        @yield('content') {{-- 主要內容區塊，必須由子模板覆寫 --}}
     </div>
 </body>
 
--- resources/views/student/child.blade.php
+++ resources/views/student/child.blade.php
@@ -1,16 +1,23 @@
 <!-- resources/views/child.blade.php -->
 
+{{--
+    這是子模板，繼承 resources/views/layouts/app.blade.php 主版型。
+    @extends('layouts.app') 代表套用主版型。
+    @section('title', 'Child Title') 用來設定主版型 <title> 的內容，對應 @yield('title')。
+    @section('sidebar') 可覆寫主版型 sidebar 區塊，@parent 會保留主版型內容。
+    @section('content') 則覆寫主版型 content 區塊。
+--}}
+
 @extends('layouts.app')
 
-@section('title', 'Child Title')
+@section('title', 'Child Title') {{-- 設定主版型 <title> 內容 --}}
 
-@section('sidebar')
-    @@parent
-
+@section('sidebar') {{-- 覆寫主版型 sidebar，@parent 保留原內容 --}}
+    @parent
     <p>This is appended to the master sidebar.</p>
 @endsection
 
-@section('content')
+@section('content') {{-- 覆寫主版型 content 區塊 --}}
     <p>This is my body content.</p>
     <p>This is my body content.</p>
     <p>This is my body content.</p>
